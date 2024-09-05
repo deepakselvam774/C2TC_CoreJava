94bebc2a7f67157e4cc82947cc7aa425a3933826
deepa <deepa@DEEPAK>
2024-09-05 09:13:55 +0530

    Looping in Java

---
+++ C2TC_Deepak/src/admin/deepak/project/Loops.java
@@ -0,0 +1,28 @@
+package admin.deepak.project;
+
+public class Loops {
+	public static void main(String[] args) {
+		// For loop
+		for (int i=0;i<10;i++)
+		{
+			System.out.println("i= "+i); 
+		}
+		int j=0;
+		while(j<10)
+		{
+			System.out.println("j= "+j);
+			j++;
+		}	
+		int k=0;
+		do
+		{
+			System.out.println("k= "+k);
+			k++;
+		
+		}
+		while(k<10);
+		}
+
+	}
+
+
